bdaf9b2a6fb1d5757e62e0036df821317fd67243
Vitalik Hakim <73163003+Vitalik-Hakim@users.noreply.github.com>
2024-11-18 19:08:31 +0000

    Update site.config.ts

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,12 +10,12 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	site: 'https://blog.superate.app', // Write here your website url
+	author: 'Superate', // Site author
+	title: 'Superate Blog', // Site title.
+	description: 'Monetize your Social Media Following through Brand Deals', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	paginationSize: 20 // Number of posts per page
 }
